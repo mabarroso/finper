a2a450f49a57a04894d5fd7194809df5d8139645
mabarroso <123025+mabarroso@users.noreply.github.com>
2025-03-02 23:56:40 +0100

    Refactoring

---
+++ src/Controller/Api/AbstractBaseController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+abstract class AbstractBaseController extends AbstractController
+{
+
+	protected function errorNotFoundResponse(string $code = 'd37e1ccf55d76c5d', array $data = []): JsonResponse
+	{
+		$data['status'] = 'error';
+		$data['code'] = $code;
+		$data['message'] = $data['message'] ?? 'Not Found';
+		return $this->json($data, Response::HTTP_NOT_FOUND);
+	}
+
+	protected function errorBadRequestResponse(string $code = '9f984d147f15e395', array $data = []): JsonResponse
+	{
+		$data['status'] = 'error';
+		$data['code'] = $code;
+		$data['message'] = $data['message'] ?? 'Bad Request';
+		return $this->json($data, Response::HTTP_BAD_REQUEST);
+	}
+
+	protected function errorConflictResponse(string $code = 'f4317cc85d531cc8', array $data = []): JsonResponse
+	{
+		$data['status'] = 'error';
+		$data['code'] = $code;
+		$data['message'] = $data['message'] ?? 'Conflict';
+		return $this->json($data, Response::HTTP_CONFLICT);
+	}
+
+}
--- src/Controller/Api/HomeController.php
+++ src/Controller/Api/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Controller\Api;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class HomeController extends AbstractBaseController
 {
+	private EntityManagerInterface $entityManager;
+
+	public function __construct(EntityManagerInterface $EntityManager)
+	{
+		$this->entityManager = $EntityManager;
+
+	}
+
+	#[Route('/api', methods: ['GET'])]
 	#[Route('/api/', methods: ['GET'])]
     public function index(): JsonResponse
     {
@@ -16,6 +25,7 @@ final class HomeController extends AbstractController
         ]);
     }
 
+	#[Route('/api/v1', methods: ['GET'])]
 	#[Route('/api/v1/', methods: ['GET'])]
 	public function indexV1(): JsonResponse
 	{
