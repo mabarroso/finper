17a2cb7a2a85229308819aa8b5f0c454aaa6eb1c
mabarroso <123025+mabarroso@users.noreply.github.com>
2025-03-07 15:49:12 +0100

    Refresh token

--- src/Controller/Api/AuthController.php
+++ src/Controller/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Dto\LoginDto;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,43 +27,67 @@ final class AuthController extends AbstractAuthBaseController
 	public function create(Request $request, UserPasswordHasherInterface $passwordHasher, #[MapRequestPayload] LoginDto $dto): JsonResponse
 	{
 		$user = $this->entityManager->getRepository(User::class)->findOneBy(["email" => $dto->email]);
-
 		if (!$user) {
 			return $this->errorForbiddenResponse('528970482d36c8c1');
 		}
 
 		if ($passwordHasher->isPasswordValid($user, $dto->password)) {
-			$date = date_create(date('Y-m-d'));
-			$timestamp = date_add($date, date_interval_create_from_date_string('1 days'));
-			$expiration = strtotime($timestamp->format('Y-m-d'));
-
-			$payload = [
-				'iss' => $request->getUriForPath(""),
-				'aud' => $user->getId(),
-				'iat' => time(),
-				'exp' => $expiration
-			];
-
-			$apiToken = JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS512');
 			return $this->json(
 				[
 					'id'    => $user->getId(),
-					'token' => $apiToken
+					'token' => $this->getApiToken($user->getId(), $request->getUriForPath(''))
 				], Response::HTTP_OK);
 		} else {
 			return $this->errorForbiddenResponse('30948930db39649a');
 		}
 	}
 
-	#[Route('/api/v{_version}/auth', requirements: ['_version' => '1'], methods: ['PUT'])]
-	public function update(): JsonResponse
+	#[Route('/api/v{_version}/auth/refreshToken', requirements: ['_version' => '1'], methods: ['PUT'])]
+	public function update(Request $request): JsonResponse
 	{
-		// todo refresh current token
+		$apiToken = $request->headers->get('TOKEN');
+		if (null === $apiToken) {
+			return $this->errorForbiddenResponse('9232a234b9607aa2fe25');
+		}
+
+		$apiTokenDecoded = JWT::decode($apiToken, new Key($_ENV['JWT_SECRET'], 'HS512'));
+		$user = $this->entityManager->getRepository(User::class)->findOneBy(['id' => $apiTokenDecoded->aud]);
+		if (!$user) {
+			return $this->errorForbiddenResponse('528970482d36c8c1');
+		} else {
+			return $this->json(
+				[
+					'id'    => $user->getId(),
+					'token' => $this->getApiToken($user->getId(), $apiTokenDecoded->iss)
+				], Response::HTTP_OK);
+		}
 	}
 
 	#[Route('/api/v{_version}/auth', requirements: ['_version' => '1'], methods: ['DELETE'])]
 	public function destroy(): JsonResponse
 	{
 		// todo invalidates all tokens
+		return $this->json([], Response::HTTP_NOT_IMPLEMENTED);
+	}
+
+	/**
+	 * @param string $uriForPath
+	 * @param int    $userId
+	 * @return string
+	 */
+	protected function getApiToken(int $userId, string $uriForPath = ''): string
+	{
+		$date = date_create(date('Y-m-d'));
+		$timestamp = date_add($date, date_interval_create_from_date_string('1 days'));
+		$expiration = strtotime($timestamp->format('Y-m-d'));
+
+		$payload = [
+			'iss' => $uriForPath,
+			'aud' => $userId,
+			'iat' => time(),
+			'exp' => $expiration
+		];
+
+		return JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS512');
 	}
 }
--- tests/Functional/Controller/Api/AuthControllerTest.php
+++ tests/Functional/Controller/Api/AuthControllerTest.php
@@ -2,7 +2,6 @@
 
 namespace App\Tests\Functional\Controller\Api;
 
-use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 
 final class AuthControllerTest extends ApiTestCase
@@ -47,5 +46,45 @@ final class AuthControllerTest extends ApiTestCase
 				$expectedData,
 				$this->getBrowserClient()->getResponse()->getContent());
 		}
+	}
+
+	public function testRefreshToken_ok(): void
+	{
+		$this->getBrowserClient()->request('PUT', '/api/v1/auth/refreshToken', [], [], ['HTTP_TOKEN' => $this->getApiToken()]);
+
+		self::assertResponseIsSuccessful();
+	}
+
+	public function testRefreshToken_ko_expired_token(): void
+	{
+		$this->getBrowserClient()->request('PUT', '/api/v1/auth/refreshToken', [], [], ['HTTP_TOKEN' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ0ZXN0IiwiYXVkIjoxLCJpYXQiOjE3NDEzNDY5NjIsImV4cCI6MTc0MTIxOTIwMH0.BeM7HoghuxzdJgfMNsCgRfoIlZ8iyRNPdUyQcbudhZcBFBSPHOD_6gYLQxNqOO77WuCcH8HX4DAA74RhkILg7A']);
+
+		self::assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+		self::assertResponseHeaderSame('content-type', 'application/json');
+		self::assertJsonEquals(
+			'{"message":"Invalid credentials."}',
+			$this->getBrowserClient()->getResponse()->getContent());
+	}
+
+	public function testRefreshToken_ko_bad_token(): void
+	{
+		$this->getBrowserClient()->request('PUT', '/api/v1/auth/refreshToken', [], [], ['HTTP_TOKEN' => 'bad_token']);
+
+		self::assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+		self::assertResponseHeaderSame('content-type', 'application/json');
+		self::assertJsonEquals(
+			'{"message":"Invalid credentials."}',
+			$this->getBrowserClient()->getResponse()->getContent());
+	}
+
+	public function testRefreshToken_ko_no_token(): void
+	{
+		$this->getBrowserClient()->request('PUT', '/api/v1/auth/refreshToken');
+
+		self::assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+		self::assertResponseHeaderSame('content-type', 'application/json');
+		self::assertJsonEquals(
+			'{"status": "error","code": "2ccd7f275e20fbd9fb01","message": "Full authentication is required to access this resource."}',
+			$this->getBrowserClient()->getResponse()->getContent());
 	}
 }
